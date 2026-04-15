02f2e191e01a8ffbf4846163fa3291b78e55c53e
Zhonglu <wangzhonglu@gmail.com>
2026-04-14 22:15:11 -0500

    Config github page

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  url: 'https://lightflare.local',
+  url: 'https://shzlw.github.io/lightflare',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
